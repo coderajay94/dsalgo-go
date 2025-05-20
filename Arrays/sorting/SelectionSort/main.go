81e4c004b5961cee3777d964df4a992c3ad0080e
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-05-20 18:44:51 +0530

    selection sort

--- Arrays/sorting/SelectionSort/main.go
+++ Arrays/sorting/SelectionSort/main.go
@@ -6,11 +6,36 @@ import (
 
 func main() {
 
-	//nums := []int{12, 1, 34, 9, 33, 6}
-	nums := []int{1}
+	nums := []int{12, 1, 34, 9, 33, 6}
+	//nums := []int{1}
 
 	fmt.Println("before sorting", nums)
 	fmt.Println("after sorting ", selectionSort(nums))
+	fmt.Println("after sorting ", selectionSortWithPositives(nums))
+}
+
+// note: we have found lowest no and then its position that is starting of the array
+// you can also find the greatest no and its position that is last
+// nums := []int{12, 1, 34, 9, 33, 6}
+func selectionSortWithPositives(nums []int) []int {
+	length := len(nums)
+
+	for i := 0; i < length-1; i++ {
+		max := findMaximum(nums, length-i)
+		nums[max], nums[length-i-1] = nums[length-i-1], nums[max]
+		fmt.Println("array after swap:", nums)
+	}
+	return nums
+}
+
+func findMaximum(nums []int, end int) int {
+	max := 0
+	for i := 1; i < end; i++ {
+		if nums[i] > nums[max] {
+			max = i
+		}
+	}
+	return max
 }
 
 func selectionSort(nums []int) []int {
