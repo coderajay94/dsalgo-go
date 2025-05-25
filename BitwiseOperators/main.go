22c7fa34fa4132809ce9eda227dc27d45b263477
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-05-25 17:48:29 +0530

    bit wise and

---
+++ BitwiseOperators/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//whenever you and anything with , it will give the same result
+	// 111001 & 111111 = result in same number as 111001
+	//so to check even odd we just need to and it with one and it will give us the last digit in binary
+	//that is responsible for even or odd
+	fmt.Println("testing bitwise operator")
+	fmt.Println(isOdd(12))
+	fmt.Println(isOdd(1))
+	fmt.Println(isOdd(0))
+}
+
+func isOdd(n int) bool {
+	return n&1 == 1
+}
