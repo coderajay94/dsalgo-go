be320f511055d448f02dc791aa585a9f7ad0e4ed
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-06-05 23:00:02 +0530

    basic recursion questions

--- Recursion/PrintN/main.go
+++ Recursion/PrintN/main.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	PrintMe(6)
-	fmt.Println()
-	PrintMeReverse(6)
-	fmt.Println(Factorial(6))
+	// PrintMe(6)
+	// fmt.Println()
+	// PrintMeReverse(6)
+	// fmt.Println(Factorial(6))
+	fmt.Println(SumOfDigits(12345))
+}
+
+func SumOfDigits(n int) int {
+	if n <= 0 {
+		return 0
+	}
+	digit := n % 10
+	return digit + SumOfDigits(n/10)
 }
 
 func Factorial(n int) int {
