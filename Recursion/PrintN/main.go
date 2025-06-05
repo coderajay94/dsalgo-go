273b560a827948679a4b89828f7d90b29dd2a316
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-06-05 22:42:37 +0530

    factorial using recursion

--- Recursion/PrintN/main.go
+++ Recursion/PrintN/main.go
@@ -4,12 +4,32 @@ import "fmt"
 
 func main() {
 	PrintMe(6)
+	fmt.Println()
+	PrintMeReverse(6)
+	fmt.Println(Factorial(6))
+}
+
+func Factorial(n int) int {
+	if n == 1 {
+		return 1
+	}
+
+	return n * Factorial(n-1)
 }
 
 func PrintMe(n int) {
-	fmt.Println(n)
-	if n == 1 {
+
+	if n == 0 {
 		return
 	}
+	fmt.Println(n)
 	PrintMe(n - 1)
+}
+
+func PrintMeReverse(n int) {
+	if n == 0 {
+		return
+	}
+	PrintMeReverse(n - 1)
+	fmt.Println(n)
 }
