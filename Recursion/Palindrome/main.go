7aae7ccb8a24488fb9a94e681865909c9339b200
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-06-06 09:23:04 +0530

    palindrome

--- Recursion/Palindrome/main.go
+++ Recursion/Palindrome/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Println("lets check palindome number")
+	target := 12421
+	fmt.Println("lets check palindome number", target)
+	fmt.Println("lets check palindome number", palindome(target))
+
+}
+
+func palindome(n int) bool {
+	countdigits := int(math.Log10(math.Abs(float64(n))) + 1)
+	return n == helper(n, countdigits)
+}
+
+func helper(n int, digits int) int {
+
+	if n == 0 {
+		return 0
+	}
+
+	last := n % 10
+	digits = digits - 1
+	return last*int(math.Pow10(digits)) + helper(n/10, digits)
 }
