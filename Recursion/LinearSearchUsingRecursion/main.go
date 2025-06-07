469e2006146856613a97c134272fbf47c84412b5
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-06-07 13:52:30 +0530

    all occurances of a number in linear search

---
+++ Recursion/LinearSearchUsingRecursion/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nums := []int{1, 2, 5, 7, 3, 3, 11, 3}
+	target := 14
+
+	fmt.Println("before linear search using recursion", nums)
+	fmt.Println("after linear search using recursion", linearSearch(nums, target, 0, len(nums)-1))
+	fmt.Println("all matched elements as target", linearSearchAllOccurances(nums, 3, 0))
+}
+
+var resp = []int{}
+
+// get all the occurances of the target
+func linearSearchAllOccurances(nums []int, target, start int) []int {
+	if start == len(nums) {
+		return resp
+	}
+
+	if nums[start] == target {
+		resp = append(resp, start)
+	}
+	return linearSearchAllOccurances(nums, target, start+1)
+}
+
+func linearSearch(nums []int, target int, start, end int) int {
+	if start > end {
+		return -1
+	}
+
+	if nums[start] == target {
+		return start
+	}
+	return linearSearch(nums, target, start+1, end)
+}
