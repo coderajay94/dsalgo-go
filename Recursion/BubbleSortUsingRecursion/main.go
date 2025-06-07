efcb7c80c341c2def01e930c25e62f785d2fb62d
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-06-07 18:34:13 +0530

    selection sort using recursion

--- Recursion/BubbleSortUsingRecursion/main.go
+++ Recursion/BubbleSortUsingRecursion/main.go
@@ -8,8 +8,30 @@ func main() {
 	nums := []int{0, 2, 88, 1, 99}
 	fmt.Println("bubble sort using recursion", nums)
 	fmt.Println("bubble sort using recursion", bubbleSort(nums, 0, len(nums)-1))
+	fmt.Println("selection sort using recursion", selectionSort(nums, 0, len(nums)-1, 0))
 }
 
+//selection sort
+// find the max then swap with the last element
+
+func selectionSort(nums []int, start, end int, max int) []int {
+	if end == 0 {
+		return nums
+	}
+
+	if start < end {
+		if nums[start+1] > nums[max] {
+			max = start + 1
+		}
+		return selectionSort(nums, start+1, end, max)
+	}
+
+	nums[max], nums[end] = nums[end], nums[max]
+	return selectionSort(nums, 0, end-1, 0)
+}
+
+// bubble sort
+// swap till the largest reaches end
 func bubbleSort(nums []int, start int, end int) []int {
 
 	if end == 0 {
