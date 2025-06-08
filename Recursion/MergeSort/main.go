5bf22890ffc2544098dd5ba29a5bc5c435757a25
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-06-09 00:05:43 +0530

    leetcode code

--- Recursion/MergeSort/main.go
+++ Recursion/MergeSort/main.go
@@ -2,12 +2,108 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 	nums := []int{5, 4, 8, 1, 9}
-	fmt.Println("before sorting array ", nums)
-	fmt.Println("after sorting array ", mergeSort(nums))
+	// fmt.Println("before sorting array ", nums)
+	// fmt.Println("after sorting array ", mergeSort(nums), 0, len(nums))
+	mergeSortInPlace(nums, 0, len(nums))
+	fmt.Println("after sorting array", nums)
+
+	testSlice(nums)
+	fmt.Println(nums)
+
+	m := make(map[string]string)
+	m["ajay"] = "kumar"
+	m["raghu"] = "kumar"
+	fmt.Println(m)
+	changeMap(m)
+	fmt.Println(m)
+	fmt.Println(digitCount("1210"))
+}
+
+func digitCount(num string) bool {
+
+	m := make(map[int]int)
+
+	for _, value := range num {
+		numb, _ := strconv.Atoi(string(value))
+		fmt.Println("iterating num", numb)
+		m[numb]++
+	}
+
+	for i := 0; i < len(num); i++ {
+
+		count, _ := strconv.Atoi(string(num[i]))
+		if m[i] != count {
+			return false
+		}
+	}
+	return true
+}
+
+func changeMap(m map[string]string) {
+	m["riddhi"] = "kumaar"
+}
+
+func testSlice(nums []int) {
+	nums[0] = 0
+}
+
+func mergeSortInPlace(nums []int, start, end int) {
+
+	if end-start == 1 {
+		return
+	}
+
+	mid := start + (end-start)/2
+
+	mergeSortInPlace(nums, start, mid)
+	mergeSortInPlace(nums, mid, end)
+
+	mergeInPlace(nums, start, mid, end)
+}
+
+func mergeInPlace(nums []int, start, mid, end int) {
+
+	results := make([]int, end-start)
+	i, j, k := start, mid, 0
+
+	//compare both the arrays and put the smallest element in the results array k
+	for i < mid && j < end {
+
+		if nums[i] < nums[j] {
+			results[k] = nums[i]
+			i++
+		} else {
+			results[k] = nums[j]
+			j++
+		}
+		k++
+	}
+
+	//add the remaining elements from left array
+	for i < mid {
+		results[k] = nums[i]
+		i++
+		k++
+	}
+
+	//add the remaining elements from right array
+	for j < end {
+		results[k] = nums[j]
+		j++
+		k++
+	}
+
+	// you
+	for i := 0; i < len(results); i++ {
+		nums[start+i] = results[i]
+	}
+
+	//fmt.Println("printing results", results)
 }
 
 func mergeSort(nums []int) []int {
