80e6bc066e3027963ae150b4c70a152b5fb9df77
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-06-11 11:49:14 +0530

    sub sequence with slice

--- Recursion/Subsequence/main.go
+++ Recursion/Subsequence/main.go
@@ -3,8 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1, 2, 3}
-	fmt.Println(subsets(nums))
+	// nums := []int{1, 2, 3}
+	// fmt.Println(subsets(nums))
+
+	str := "abc"
+	fmt.Println(subseq(str))
+}
+
+func subseq(str string) []string {
+	return sub("", str, []string{})
+}
+
+func sub(pr string, un string, resp []string) []string {
+
+	if un == "" {
+		res := []string{pr}
+		return res
+	}
+
+	ch := string(un[0])
+	//resp = append(resp, ch)
+
+	left := sub(pr+ch, un[1:], resp)
+	right := sub(pr, un[1:], resp)
+	return append(left, right...)
 }
 
 func subsets(nums []int) [][]int {
