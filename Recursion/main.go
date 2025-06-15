42faa11dd0a7aa87bbff70f8c4061f951b76654b
coderajay94 <83922699+coderajay94@users.noreply.github.com>
2025-06-15 19:45:13 +0530

    Add files via upload
    adding maze problem

--- Recursion/main.go
+++ Recursion/main.go
@@ -1,15 +1,56 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-
-	PrintMe(1)
+	//user can go only down or right
+	fmt.Println("Maze Problem")
+	//fmt.Println(solveMaze(3, 3))
+	//solveMazePath("", 3, 3)
+	fmt.Println(solveMazePathWithSlice("", 3, 3))
 }
 
-func PrintMe(n int) {
-	fmt.Println("printing ", n)
-	PrintMe(n + 1)
+func solveMaze(row, column int) int {
+
+	//even if one of row or column reached to 1, we know only one path exists now
+	if row == 1 || column == 1 {
+		return 1
+	}
+
+	left := solveMaze(row-1, column)
+	right := solveMaze(row, column-1)
+	return left + right
+}
+
+func solveMazePath(path string, row, column int) {
+
+	if row == 1 && column == 1 {
+		fmt.Println(path)
+		return
+	}
+
+	if row > 1 {
+		solveMazePath(path+"D", row-1, column)
+	}
+	if column > 1 {
+		solveMazePath(path+"R", row, column-1)
+	}
+
+}
+
+func solveMazePathWithSlice(path string, row, column int) []string {
+
+	res := []string{}
+	if row == 1 && column == 1 {
+		res = append(res, path)
+		return res
+	}
+
+	if row > 1 {
+		res = append(res, solveMazePathWithSlice(path+"D", row-1, column)...)
+	}
+	if column > 1 {
+		res = append(res, solveMazePathWithSlice(path+"R", row, column-1)...)
+	}
+	return res
 }
